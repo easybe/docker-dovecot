f0f91579aa49e8e63a7ef380711678a63bde3ee1
Ezra Buehler <spam@easyb.ch>
2016-05-08 21:48:01 +0200

    Disable debug output in startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 
 sed -i "s/##DB_PASS##/${DB_ENV_MYSQL_PASSWORD}/" /etc/dovecot/dovecot-sql.conf.ext
 sed -i "s/##HOSTNAME##/${HOSTNAME}/" /etc/dovecot/conf.d/99-local-lmtp.conf
